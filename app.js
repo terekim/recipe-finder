7d1f7d781c81e5facc6d0d945d74ec7c5fd40d45
Clayton Hung <Clayton@v1020-wn-93-54.campus-dynamic.uwaterloo.ca>
2015-01-31 03:50:33 -0500

    not sure

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 };
@@ -18,14 +18,11 @@ var allowCrossDomain = function(req, res, next) {
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
-
+	//configurations
   app.use(express.static(path.join(__dirname, 'public')));
-   app.use(allowCrossDomain);
+  app.use(allowCrossDomain);
 
-  console.log('Example app listening at http://%s:%s', host, port);
-
+	console.log("Server is running!");
 });
 
 
