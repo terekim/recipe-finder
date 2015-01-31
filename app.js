8334711e0c3553d63d86bcd1e631d1a26ba67b19
Clayton Hung <Clayton@v1020-wn-93-54.campus-dynamic.uwaterloo.ca>
2015-01-31 03:40:06 -0500

    redirect path to index.html

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.redirect('/index.html');
 });
+
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
 
 var server = app.listen(3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
+  app.use(express.static(path.join(__dirname, 'public')));
+   app.use(allowCrossDomain);
+
   console.log('Example app listening at http://%s:%s', host, port);
 
-})
+});
+
+
