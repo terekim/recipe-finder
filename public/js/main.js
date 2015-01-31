5d99fa9a1ecd8d774ba3bd8c3083be8ec1542d1a
Terence Kim <terencekim@Terences-MacBook-Pro.local>
2015-01-31 14:07:35 -0500

    added sidebar

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,30 @@
+function populateSearch(profileData){
+
+}
+
+function fadeToSearch(){
+  $("#splash").fadeOut(2000)
+  $("#search").fadeIn(2000)
+
+  // $(".splash").css("background", "#E1E3D7")
+
+
+}
 function transitionToSearch(fbProfile) {
   var id = fbProfile.id;
   var firstName = fbProfile.first_name;
   var lastName = fbProfile.last_name;
-  debugger;
-  $.ajax({
-    type: "GET",
-    url: 'http://localhost:3000/api',
-    data: { id: id, firstname: firstName, lastname: lastName }
-  })
-    .done(function( msg ) {
-      alert( "Data Saved: " + msg );
-    });
+  var profileData = { id: id, firstname: firstName, lastname: lastName };
+  // $.ajax({
+  //   type: "GET",
+  //   url: 'http://localhost:3000/api',
+  //   data: profileData
+  // })
+  // .done(function( msg ) {
+  //   var recipeFinderUserData = msg
+  //   alert( "Data Retrieved: " + msg );
+  // });
+  console.log("AJAX REQUEST SENT TO LOCALHOST:3000");
+  populateSearch(profileData); //this needs to be in .done callback
+  fadeToSearch();
 }
